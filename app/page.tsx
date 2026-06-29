2748483961ba784ca5bde7015c5cf45ba8739469
sameerkhan86926-a11y <sameerkhan86926@gmail.com>
2026-06-29 18:34:42 +0530

    Add advertisement sections to the main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Script from "next/script"
+
 import { SiteHeader } from "@/components/site-header"
 import { SiteFooter } from "@/components/site-footer"
 import { Hero } from "@/components/hero"
@@ -13,19 +14,47 @@ export default function HomePage() {
       <SiteHeader />
 
       <main className="flex-1">
+
+        {/* HERO */}
         <Hero />
 
-        {/* ADS PLACE HERE 👇 */}
-        <Script
-          src="https://pl30123077.effectivecpmnetwork.com/c483387c5ce2665f5c8dc04518be2c7c/invoke.js"
-          strategy="afterInteractive"
-        />
-        <div id="container-c483387c5ce2665f5c8dc04518be2c7c"></div>
+        {/* 🔥 AD 1 (after hero - high CTR) */}
+        <div className="my-6 flex justify-center">
+          <Script
+            src="https://pl30123077.effectivecpmnetwork.com/c483387c5ce2665f5c8dc04518be2c7c/invoke.js"
+            strategy="afterInteractive"
+          />
+          <div id="container-c483387c5ce2665f5c8dc04518be2c7c"></div>
+        </div>
 
+        {/* POPULAR TOOLS */}
         <PopularTools />
+
+        {/* 🔥 AD 2 (middle - best revenue spot) */}
+        <div className="my-10 flex justify-center">
+          <div
+            dangerouslySetInnerHTML={{
+              __html: `
+                <script>
+                  atOptions = {
+                    'key' : 'ecc4afc5b67ca88efe9cb93a11d4b9c5',
+                    'format' : 'iframe',
+                    'height' : 250,
+                    'width' : 300,
+                    'params' : {}
+                  };
+                </script>
+                <script src="https://www.highperformanceformat.com/ecc4afc5b67ca88efe9cb93a11d4b9c5/invoke.js"></script>
+              `,
+            }}
+          />
+        </div>
+
+        {/* बाकी sections */}
         <CategoriesSection />
         <FeaturesSection />
         <FaqSection />
+
       </main>
 
       <SiteFooter />
