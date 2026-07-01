1164df070a1137b02726cc31a51abee850910487
sameerkhan86926-a11y <sameerkhan86926@gmail.com>
2026-07-01 11:03:44 +0530

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,7 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: '*',
       allow: '/',
     },
-    sitemap: 'https://toolboxar-project.vercel.app/sitemap.xml',
+    sitemap: 'https://pdftoolbox.shop/sitemap.xml',
+    host: 'https://pdftoolbox.shop',
   }
 }
