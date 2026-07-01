96bdc1b989cb9dbb55718f803b4bb49c60d8624d
sameerkhan86926-a11y <sameerkhan86926@gmail.com>
2026-07-01 10:57:45 +0530

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://toolboxar-project.vercel.app'
+  const baseUrl = 'https://pdftoolbox.shop'
 
   const pages = [
     '',
